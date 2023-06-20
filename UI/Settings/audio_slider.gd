3331506d7dbc33a93b0c5e36de8b26c0e442ce67
venex-source <venex248@gmail.com>
2023-06-20 12:57:42 +0100

    Fixed Bug

--- UI/Settings/audio_slider.gd
+++ UI/Settings/audio_slider.gd
@@ -2,21 +2,21 @@ extends VBoxContainer
 class_name audio_slider
 
 var music_bus = AudioServer.get_bus_index("Music")
-var sound_fx_bus = AudioServer.get_bus_index("SoundFX")
+var sound_fx_bus = AudioServer.get_bus_index("SoundFx")
 
 
 func _on_MusicSlider_value_changed(value):
-	set_bus_volume(music_bus, value)
+	change_bus_volume(music_bus, value)
 
 
 func _on_SoundFxSlider_value_changed(value):
-	set_bus_volume(sound_fx_bus, value)
+	change_bus_volume(sound_fx_bus, value)
 
 
-func set_bus_volume(audio_bus, value) -> void:
+func change_bus_volume(audio_bus, value) -> void:
 	AudioServer.set_bus_volume_db(audio_bus, value)
 	
-	if value == -30:
+	if value == -5:
 		AudioServer.set_bus_mute(audio_bus, true)
 	else:
 		AudioServer.set_bus_mute(audio_bus, false)
