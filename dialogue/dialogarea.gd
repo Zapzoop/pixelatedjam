10e4e8cba8ea4a6adee7f53d5b0fd24e9de0f4f3
Zapzoop <136953483+Zapzoop@users.noreply.github.com>
2023-06-20 19:51:57 +0530

    fixed inconsistency

--- dialogue/dialogarea.gd
+++ dialogue/dialogarea.gd
@@ -6,12 +6,12 @@ var area_active = false
 
 func _input(event):
 	if area_active and event.is_action_pressed("action"):
+		print("Emitted")
 		Signalbus.emit_signal("display_dialog", text_key)
 
 
 func _on_dialoguearea_area_entered(area):
 	area_active = true
-
 
 func _on_dialoguearea_area_exited(area):
 	area_active = false
