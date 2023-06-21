5303efb82e391b015a7893b819399afdc13e85dd
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2023-06-21 14:36:56 +0800

    Monster tweaks * Added new attack patterns for the 'Boss' mob

--- Enemies/Boss/Boss.gd
+++ Enemies/Boss/Boss.gd
@@ -9,14 +9,17 @@ const RAGE_AOE_THRESH = 0.1
 
 export var ACCELERATION = 70
 export var MAX_SPEED = 40
-export var FRICTION = 140
+export var FRICTION = 170
 export var WANDER_TARGET_RANGE = 5
+
+export var SHOW_HEALTHBAR := false
+
 export var NEAR_DISTANCE = 90.0
-export var DASH_COOLDOWN = 5.0
-export var DASH_FORCE = 38.0
+export var DASH_COOLDOWN = 5.5
+export var DASH_FORCE = 43.0
 export var CAST_COOLDOWN = 6.0
-export var SPAWN_COOLDOWN = 15.0
-export var SHOOT_COOLDOWN = 2.0
+export var SPAWN_COOLDOWN = 20.0
+export var SHOOT_COOLDOWN = 12.0
 export var SHOOT_CHANCE = 0.35
 
 export var pkg_mine: PackedScene
@@ -49,20 +52,33 @@ onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
+onready var hitbox = $Hitbox
 onready var softCollision = $SoftCollision
 onready var wanderController = $WanderController
 onready var animationPlayer = $AnimationPlayer
 onready var footstep_fx = $FootstepEffect
 onready var projectile_origin = $projectile_spawn_point
+onready var spawn_parent = get_parent()
+
+onready var healthbar = get_node("%BossHealthBar")
 
 func _ready():
 	state = pick_random_state([IDLE, WANDER])
 
 func _process(delta: float) -> void:
+	var is_idle = state == IDLE || state == SUMMON || state == CAST
+	
+	footstep_fx.is_active = !is_idle
+	sprite.playing = !is_idle
+	
 	rage = max(0.0, rage - (0.035 * delta))
 	
 	if !playerDetectionZone.can_see_player():
+		healthbar.visible = false
 		return
+	
+	if SHOW_HEALTHBAR:
+		healthbar.visible = true
 
 	if rage > RAGE_DASH_THRESH:
 		dash_cooldown = max(0.0, dash_cooldown - delta)	
@@ -91,9 +107,6 @@ func _physics_process(delta):
 	
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback + force)
-	
-	footstep_fx.is_active = state != IDLE
-	sprite.playing = state != IDLE
 	
 	match state:
 		IDLE:
@@ -128,23 +141,31 @@ func _physics_process(delta):
 				if rage < RAGE_SPAM_THRESH:
 					dash_cooldown = DASH_COOLDOWN
 				else:
-					dash_cooldown = DASH_COOLDOWN * 0.5
+					# Chance to chain into other attacks
+					if rand_range(0.0, 1.0) < 0.15:
+						if rand_range(0.0, 1.0) <= 0.5:
+							dash_cooldown = 0.5
+						else:
+							cast_cooldown = 0.5
+					else:
+						dash_cooldown = DASH_COOLDOWN * 0.5
 				
 				var dash_dir = global_position.direction_to(player_position)
-				force += dash_dir * -24.0
+				force += dash_dir * -25.0
 				
-				for _frame in range(125):
+				for _frame in range(150):
 					yield(tree, "idle_frame")
+				
+				# Chance to spawn a trail of AOE blasts
+				if rage > RAGE_SPAM_THRESH && rand_range(0.0, 1.0) < 0.35:
+					_start_aoe_attack(1)
 				
 				var dash_bias = dash_dir.rotated(PI * rand_range(-0.5, 0.5))
 				
 				dash_dir = dash_dir.linear_interpolate(dash_bias, rand_range(0.1, 0.35))
 				force += dash_dir * DASH_FORCE
 				
-				var mine = pkg_mine.instance()
-				owner.get_parent().add_child(mine)
-				
-				mine.global_position = global_position
+				_spawn_mine()
 
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 150
@@ -181,8 +202,12 @@ func _on_Hurtbox_area_entered(area):
 	knockback = area.knockback_vector * 95
 	hurtbox.create_hit_effect()
 	hurtbox.start_invincibility(0.4)
-	
-	rage = min(10.0, rage + area.damage * 0.15)
+
+	# Force rage in low health
+	if (float(stats.health) / float(stats.max_health)) <= 0.4:
+		rage = 10.0
+	else:
+		rage = min(10.0, rage + area.damage * 0.15)	
 
 func _on_Stats_no_health():
 	queue_free()
@@ -201,42 +226,11 @@ func _on_animation_frame_changed() -> void:
 		return
 	
 	if state == SUMMON && sprite.frame == 3:
-		var minion_count: int
-		
-		if rage < RAGE_SPAM_THRESH:
-			spawn_cooldown = SPAWN_COOLDOWN
-			minion_count = 1 + randi() % 4
-		else:
-			spawn_cooldown = SPAWN_COOLDOWN * 0.75
-			minion_count = 2 + randi() % 4
-		
-		var current_position = global_position
-		var spawn_parent = get_parent()
-		
-		var tree = get_tree()
-		
-		for _count in range(minion_count):
-			var minion = pkg_minion.instance()
-			spawn_parent.add_child(minion)
-			
-			var spawn_offset = Vector2(
-				rand_range(-32.0, 32.0),
-				rand_range(-32.0, 32.0)
-			)
-			
-			var spawn_position = current_position + spawn_offset
-			var effect = pkg_spawn_effect.instance()
-		
-			spawn_parent.add_child(effect)
-			
-			effect.global_position = spawn_position
-			minion.global_position = spawn_position
-			
-			for _frame in range(24):
-				yield(tree, "idle_frame")
+		spawn_cooldown = SPAWN_COOLDOWN
+		_spawn_minions()
 
 	if state == CAST && sprite.frame == 5:
-		_spawn_aoe_attack(randi() % 4)
+		_start_aoe_attack(randi() % 4)
 		
 		if rage < RAGE_SPAM_THRESH:
 			cast_cooldown = CAST_COOLDOWN
@@ -247,6 +241,10 @@ func _on_animation_finished() -> void:
 	if state != CAST && state != SUMMON:
 		return
 	
+	force = Vector2.ZERO
+	velocity = Vector2.ZERO
+	knockback = Vector2.ZERO
+	
 	state = IDLE
 	_set_lock_state(true)
 
@@ -254,7 +252,7 @@ func _set_lock_state(is_unlocked: bool) -> void:
 	set_process(is_unlocked)
 	set_physics_process(is_unlocked)
 
-func _spawn_aoe_attack(type: int) -> void:
+func _start_aoe_attack(type: int) -> void:
 	"""
 	AOE Attack type
 	0: expanding
@@ -265,7 +263,6 @@ func _spawn_aoe_attack(type: int) -> void:
 	if !is_instance_valid(playerDetectionZone.player):
 		return
 	
-	var spawn_parent = get_parent()
 	var tree = get_tree()
 	
 	match type:
@@ -278,20 +275,17 @@ func _spawn_aoe_attack(type: int) -> void:
 			]
 			
 			var distance = 1.0
-			var cycles = 5 if rage > RAGE_SPAM_THRESH else 3
+			var cycles = 12 if rage > RAGE_SPAM_THRESH else 3
 			var caster_pos = global_position
 			
 			for _cycle in range(cycles):
 				for dir in dirs:
 					for i in [-1.0, 1.0]:
-						var aoe_attack = pkg_aoe_attack.instance()
-						spawn_parent.add_child(aoe_attack)
-						
-						aoe_attack.global_position = caster_pos + (dir * i) * 16.0 * distance
+						_spawn_aoe_blast(caster_pos + (dir * i) * 16.0 * distance)
 				
 				distance += 1.5
 				
-				for _frame in range(24):
+				for _frame in range(30):
 					yield(tree, "idle_frame")
 		
 		1:
@@ -300,32 +294,37 @@ func _spawn_aoe_attack(type: int) -> void:
 			
 			var caster_pos = global_position			
 			var target_dir = caster_pos.direction_to(playerDetectionZone.player.global_position)
+			var rage_mode = rage > RAGE_SPAM_THRESH
 			
 			for cycle in range(cycles):
-				var aoe_attack = pkg_aoe_attack.instance()
-				spawn_parent.add_child(aoe_attack)
-				
-				var spawn_bias = Vector2(
-					rand_range(-1.0, 1.0),
-					rand_range(-1.0, 1.0)
-				).normalized()
-				
-				aoe_attack.global_position = caster_pos + (target_dir + spawn_bias * rand_range(0.05, 0.25)) * 8.0 * cycle
+				if !rage_mode:
+					var spawn_bias = Vector2(
+						rand_range(-1.0, 1.0),
+						rand_range(-1.0, 1.0)
+					).normalized()
+					
+					_spawn_aoe_blast(caster_pos + (target_dir + spawn_bias * rand_range(0.05, 0.25)) * 8.0 * cycle)
+				else:
+					for _x in range(5):
+						var spawn_bias = Vector2(
+							rand_range(-1.5, 1.5),
+							rand_range(-1.5, 1.5)
+						).normalized()
+						
+						_spawn_aoe_blast(caster_pos + (target_dir + spawn_bias * rand_range(0.05, 0.25)) * 32.0 * cycle)
 				
 				for _frame in range(24):
 					yield(tree, "idle_frame")
 		
 		2:
-			var cycles = 5 if rage > RAGE_SPAM_THRESH else 13
+			var cycles = 3 if rage < RAGE_SPAM_THRESH else 5
+			_spawn_mine()
 			
 			for cycle in range(cycles):
-				var aoe_attack = pkg_aoe_attack.instance()
-				spawn_parent.add_child(aoe_attack)
-				
 				if !is_instance_valid(playerDetectionZone.player):
 					return
 				
-				aoe_attack.global_position = playerDetectionZone.player.global_position
+				_spawn_aoe_blast(playerDetectionZone.player.global_position)
 				
 				for _frame in range(150):
 					yield(tree, "idle_frame")
@@ -350,6 +349,12 @@ func _spawn_aoe_attack(type: int) -> void:
 				for _frame in range(cycle_speed):
 					yield(tree, "idle_frame")
 
+func _spawn_aoe_blast(position: Vector2) -> void:
+	var aoe_attack = pkg_aoe_attack.instance()
+	spawn_parent.add_child(aoe_attack)
+	
+	aoe_attack.global_position = position
+
 func _shoot_tracking_projectiles() -> void:
 	if rage < RAGE_SPAM_THRESH:
 		shoot_cooldown = SHOOT_COOLDOWN
@@ -359,19 +364,21 @@ func _shoot_tracking_projectiles() -> void:
 	if rand_range(0.0, 1.0) < SHOOT_CHANCE:
 		return
 	
-	var spawn_parent = get_parent()
 	var count: int
 	
 	if rage < RAGE_SPAM_THRESH:
-		count = 3 + randi() % 5
+		count = 2 + randi() % 2
 	else:
-		count = 1 + randi() % 3
+		count = 3 + randi() % 5
 	
 	var tree = get_tree()
 	var spawn_pos = projectile_origin.global_position
 	var player = playerDetectionZone.player
 	
 	for _cycle in range(count):
+		if !is_instance_valid(player):
+			return
+		
 		var projectile = pkg_projectile.instance()
 		spawn_parent.add_child(projectile)
 		
@@ -380,3 +387,76 @@ func _shoot_tracking_projectiles() -> void:
 		if count > 1:
 			for _frame in range(32):
 				yield(tree, "idle_frame")
+
+func _spawn_mine() -> void:
+	var mine = pkg_mine.instance()
+	owner.get_parent().add_child(mine)
+	
+	mine.global_position = global_position
+
+func _spawn_minions() -> void:
+	var minion_count: int
+	
+	if rage < RAGE_SPAM_THRESH:
+		minion_count = 1 + randi() % 2
+	else:
+		minion_count = 2 + randi() % 4
+		
+		if rand_range(0.0, 1.0) < 0.4:
+			_teleport_near_player()
+	
+	var current_position = global_position
+	var tree = get_tree()
+	
+	for _count in range(minion_count):
+		var minion = pkg_minion.instance()
+		spawn_parent.add_child(minion)
+		
+		var spawn_offset = Vector2(
+			rand_range(-45.0, 45.0),
+			rand_range(-45.0, 45.0)
+		)
+		
+		var spawn_position = current_position + spawn_offset
+		var effect = pkg_spawn_effect.instance()
+	
+		spawn_parent.add_child(effect)
+		
+		effect.global_position = spawn_position
+		minion.global_position = spawn_position
+		
+		for _frame in range(24):
+			yield(tree, "idle_frame")
+
+func _teleport_near_player() -> void:		
+	var tree = get_tree()
+	
+	visible = false
+	hitbox.monitorable = false
+	hitbox.monitoring = false
+	
+	for _frame in range(200 + randi() % 250):
+		yield(tree, "idle_frame")
+	
+	var offset = Vector2(
+		rand_range(-16.0, 16.0),
+		rand_range(-16.0, 16.0)
+	)
+	
+	if !is_instance_valid(playerDetectionZone.player):
+		visible = true
+		return
+
+	var spawn_pos = playerDetectionZone.player.global_position + offset
+	
+	var effect = pkg_spawn_effect.instance()
+	spawn_parent.add_child(effect)
+	
+	visible = true
+	
+	effect.global_position = spawn_pos
+	global_position = spawn_pos
+	
+	hitbox.monitorable = true
+	hitbox.monitoring = true
+	
--- Enemies/Boss/BossTrackingProjectile.gd
+++ Enemies/Boss/BossTrackingProjectile.gd
@@ -19,6 +19,7 @@ func _process(delta: float) -> void:
 		rotation = atan2(dir.y, dir.x)
 	
 	track_time -= delta
+	position += dir * -8.0 * delta
 	
 	if track_time > 0.0:
 		return
@@ -40,7 +41,11 @@ func _activate() -> void:
 
 func _calculate_dir() -> void:
 	if !is_instance_valid(target):
+		track_time = 0.0
 		return
 	
 	var next_dir = global_position.direction_to(target.global_position)
 	dir = dir.linear_interpolate(next_dir, 0.15)
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	queue_free()
